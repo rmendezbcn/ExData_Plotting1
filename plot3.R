5eac7919d930c702470037bd7c4d59f4046ea458
Raul Mendez-Vasquez <rmendezvasquez@gmail.com>
2016-05-24 00:12:54 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,6 +9,7 @@ plot(plot_dataset$sub_metering_1, type = 'l', pch = '', xaxt = 'n', ylab = 'Ener
 axis(1, at = c(1, length(plot_dataset$global_active_power)/2, length(plot_dataset$global_active_power)), lab=c("Thu","Fri", 'sat'))
 lines(plot_dataset$sub_metering_2, col = 'red')
 lines(plot_dataset$sub_metering_3, col = 'blue')
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1,1))
 
 # copying the plot to a file device
 dev.copy(png, file = 'plot3.png')
